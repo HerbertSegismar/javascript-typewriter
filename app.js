59ca30005b8811ae955c61d9905f2143e72aed8d
Herb Segismar <herbert.segismar@yahoo.com>
2025-07-29 20:26:41 +1200

    Final commit

--- app.js
+++ app.js
@@ -6,20 +6,33 @@ let j = 0;
 let time = 500;
 
 const phrases = [
-    'Hello! ', 
-    'My name is Herb. ', 
+    'Hi there! ', 
+    'My name is Herb Segis. ', 
     'I am a Front-End Developer, ', 
-    'Back-End Developer, ', 
-    'and a Full-Stack Engineer. ',
+    'and a Back-End Developer, ', 
+
+    'I use Tailwind to style my Projects ',
+    'and JavaScript to make my pages Interactive. ',
+
+    'I am currently working with React and Node.js. ',
+    'Also VueJS and Nuxt is on my List ',
+
+    'My goal is to become a Senior Full-Stack Engineer. '
 ]
 
 function type(){
     if (i < phrases.length){
         if (!deleting && j <= phrases[i].length){
+            text.classList.add("typing");
             currentPhrase.push(phrases[i][j]);
             j++;
             text.innerHTML = currentPhrase.join('');
+        } 
+        else {
+            text.classList.remove("typing");
         }
+
+
         if (j === phrases[i].length){
             deleting = true;
             time = 200;
@@ -33,6 +46,9 @@ function type(){
             time = 500;
             i++;
             j = 0;
+        }
+        if (i === phrases.length){
+            i = 0;
         }
         
     }
