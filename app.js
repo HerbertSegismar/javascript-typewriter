899d55a3d967286872a317641d1f3d8b23ec69cf
Herb Segismar <herbert.segismar@yahoo.com>
2025-07-29 20:04:51 +1200

    randomize timeout duration

--- app.js
+++ app.js
@@ -3,29 +3,40 @@ let currentPhrase = [];
 let deleting = false;
 let i = 0;
 let j = 0;
+let time = 500;
 
 const phrases = [
     'Hello! ', 
     'My name is Herb. ', 
     'I am a Front-End Developer, ', 
     'Back-End Developer, ', 
-    'and a Full-Stack Engineer'
+    'and a Full-Stack Engineer. ',
 ]
 
 function type(){
     if (i < phrases.length){
         if (!deleting && j <= phrases[i].length){
             currentPhrase.push(phrases[i][j]);
-            text.innerHTML = currentPhrase.join('');
             j++;
+            text.innerHTML = currentPhrase.join('');
         }
         if (j === phrases[i].length){
+            deleting = true;
+            time = 200;
+        }
+        if (deleting && currentPhrase.length > 0){
+            currentPhrase.pop();
+            text.innerHTML = currentPhrase.join('');
+        }
+        if (deleting && currentPhrase.length === 0){
+            deleting = false;
+            time = 500;
             i++;
             j = 0;
         }
         
     }
-    setTimeout(type, 300);
+    setTimeout(type, time * Math.random());
 }
 
 type();
