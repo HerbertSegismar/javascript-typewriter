bb1831bae3d5c714f389854666eef4e509634fd8
Herb Segismar <herbert.segismar@yahoo.com>
2025-07-29 19:41:40 +1200

    Typing effect

--- app.js
+++ app.js
@@ -5,10 +5,10 @@ let i = 0;
 let j = 0;
 
 const phrases = [
-    'Hello!', 
-    'My name is Herb', 
-    'I am a Front-End Developer', 
-    'Back-End Developer', 
+    'Hello! ', 
+    'My name is Herb. ', 
+    'I am a Front-End Developer, ', 
+    'Back-End Developer, ', 
     'and a Full-Stack Engineer'
 ]
 
@@ -19,6 +19,11 @@ function type(){
             text.innerHTML = currentPhrase.join('');
             j++;
         }
+        if (j === phrases[i].length){
+            i++;
+            j = 0;
+        }
+        
     }
     setTimeout(type, 300);
 }
